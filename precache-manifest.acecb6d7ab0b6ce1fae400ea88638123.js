95528cba4cdf6a2fc2c02a42d5f18bd03b6fe47c
Code Sandbox <hello@codesandbox.io>
2021-07-18 05:43:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.acecb6d7ab0b6ce1fae400ea88638123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536c2a78434a1e5d0ae4bc65720824d8",
+    "url": "https://yuvinogradov.github.io/csb-oi387/index.html"
+  },
+  {
+    "revision": "91a43cbe9a0f9be87c17",
+    "url": "https://yuvinogradov.github.io/csb-oi387/static/css/main.33921b11.chunk.css"
+  },
+  {
+    "revision": "53c9a43c1f4e9d7381c2",
+    "url": "https://yuvinogradov.github.io/csb-oi387/static/js/2.8c801064.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://yuvinogradov.github.io/csb-oi387/static/js/2.8c801064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a43cbe9a0f9be87c17",
+    "url": "https://yuvinogradov.github.io/csb-oi387/static/js/main.ee599920.chunk.js"
+  },
+  {
+    "revision": "5a953762f243e668c1b6",
+    "url": "https://yuvinogradov.github.io/csb-oi387/static/js/runtime-main.4798ebf2.js"
+  }
+]);
